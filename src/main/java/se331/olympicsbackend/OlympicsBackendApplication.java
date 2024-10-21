c74b636a85bd31ce66e32819e107da48c4a69b0f
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2024-10-21 16:41:24 +0700

    first commit

---
+++ src/main/java/se331/olympicsbackend/OlympicsBackendApplication.java
@@ -0,0 +1,13 @@
+package se331.olympicsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlympicsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se331/olympicsbackend/OlympicsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.olympicsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
